dbf8fd341c424ab0f087f6a5d56803476582755b
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-02-27 13:31:11 +0500

    home page f

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
-	$('#bars').click(function() {
-		$('.nav').slideToggle().toggleClass('open');
-		console.log('ishla');
-	});
+    //$('.overlay').css('display', 'block');
 
-	$('.nav .close').click(function() {
-		('.nav').css('display','none');
-		console.log('/')
-	});
+    /*$('').click(function() {
+        //$(this).parents('.modal').slideUp(200);
+		$('.overlay').fadeOut(100);
+        $('.modal').fadeOut(100);
+    });*/
+    $('.modal .close').click(function() {
+    	$(this).parents('.modal').fadeOut();
+    });
 
-	$('#search').click(function() {
-		$('.search').slideToggle().toggleClass('open');
-		
-	})
+    $('#no').click(function() {
+        $('#modal1').fadeOut("slow");
+    });
+
+    $('#bars').click(function() {
+        $('.nav').slideToggle().toggleClass('open');
+        console.log('ishla');
+    });
+
+    $('.nav .close').click(function() {
+        ('.nav').css('display', 'none');
+        console.log('/')
+    });
+    $('#search').click(function() {
+    	$(this).toggleClass('crim');
+        $('.search').slideToggle().toggleClass('open');
+    });
+
+   	$('#language').on('click', function() {
+   		$(this).toggleClass('crim');
+   		$('#modal-language').slideToggle().toggleClass('open');
+   		
+   	})
 })
